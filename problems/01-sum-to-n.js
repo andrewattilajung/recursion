c7e69c738abddabafafd0a4d904de20cb5cfa27a
Khiienu <anthonyhuynh123@gmail.com>
2021-03-23 13:15:01 -0500

    finished sumToN

--- problems/01-sum-to-n.js
+++ problems/01-sum-to-n.js
@@ -11,8 +11,23 @@ sumToN(9)  // returns 45
 sumToN(-8)  // returns null
 ***********************************************************************/
 
-// your code here
+//! write a function named sumToN . parameter of Number/
 
+//! return sum of all number from 0 to that num .
+//! create a variable starting at 0. 
+//! if statement for base case/ the base case is keeping the function from being an infinet loop 
+    //! if(num === 0) return null. 
+//! write a return function. return number . + (variable + 1)
+
+let sumToN = number => {
+  if(number <= 0){
+    return null;
+  }
+  if(number === 1){
+    return 1;
+  }
+  return number + sumToN(number - 1)
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumToN;
