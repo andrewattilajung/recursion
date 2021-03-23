fc630a4ebfac579aa62ffe7d7d0e2d8fff807b52
Andrew Jung <andrewattilajung@gmail.com>
2021-03-23 16:04:32 -0700

    09 incomplete

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -6,7 +6,7 @@ Not familiar with the Fibonacci sequence? Beginning with 0 and 1, we add the two
 previous numbers in the sequence together to form the next one:
 
 0, 1, 1, 2, 3, 5, 8, etc....
-
+1, 2, 3, 4, 5, 6, 7
 We count Fibonacci numbers beginning with the first 1. Take a look at the
 examples below if you're unsure where to start!
 
@@ -19,12 +19,27 @@ fibonacci(4); // 3
 fibonacci(10); // 55
 ***********************************************************************/
 
-// your code here
-  
+let fibonacci = number => {
+  if(number <= 0){
+    return null;
+  }
+  if(number === 1){
+    return 1;
+  }
+  return number + fibonacci(number - 1)
+}
+
+
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(10)); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = fibonacci;
 } catch (e) {
   module.exports = null;
 }
-  
