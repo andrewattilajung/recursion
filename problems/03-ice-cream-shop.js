a9819facf99ca41091badca6433be76cddc2fbfc
Khiienu <anthonyhuynh123@gmail.com>
2021-03-23 15:25:25 -0500

    unfinished icecream push

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -22,9 +22,23 @@ if not return false
 
 ***********************************************************************/
 
+let iceCreamShop = (flavors, favorite) => {
+  if(flavors.length === 0){
+    return false; 
+  }
+  if(flavors[0] === favorite){
+    return true;
+  }
+  
+  return iceCreamShop(flavors.shift());
+}
 
-// your code here
 
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
